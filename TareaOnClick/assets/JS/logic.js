ee03b10dde81dd63206f1c2f2a09d406755a53c3
Javiera <javiera.segurav@gmail.com>
2024-07-26 01:52:37 -0400

    subiendo carpeta de tareas

---
+++ TareaOnClick/assets/JS/logic.js
@@ -0,0 +1,12 @@
+function changeName(element,text)
+{
+    element.innerText=text;
+}
+function hideElement(element)
+{
+    element.remove();
+}
+function hideElement(element)
+{
+    element.remove();
+}
